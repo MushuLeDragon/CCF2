1b3a4e6858e3ecf677d263c51dbe0765c16c42d3
MushuLeDragon <dragon.mushu@hotmail.com>
2018-02-23 15:25:06 +0100

    Modif de la température

--- Main.c
+++ Main.c
@@ -3,10 +3,51 @@
 #include "config_bits.h"
 
 
+
+int tempdebase = 30;
+int temp = tempdebase;
+
 void main(void) {
-    
-    int temp=30;
-    
-    
+
+    //Boutons de température en entrée
+    TRISCbits.TRISC0 = 1;
+    TRISCbits.TRISC1 = 1;
+
+    //LEDS en sortie
+    TRISCbits.TRISC3 = 0;
+    TRISCbits.TRISC4 = 0;
+    TRISCbits.TRISC5 = 0;
+    TRISCbits.TRISC6 = 0;
+    TRISCbits.TRISC7 = 0;
+
+    //Chauffage et ventilateur en sortie
+    TRISDbits.TRISD0 = 0;
+    TRISDbits.TRISD1 = 0;
+
+    //Capteur de température en entrée
+    TRISAbits.TRISA0 = 1;
+
+    //Allume la LED du port RC3
+    //PORTCbits.RC3 = 1;
+
+
+
     return;
+}
+
+void changeTemperature() {
+    if (PORTCbits.RC0 == 0) {
+        if (temp == 22) {
+            temp = 22;
+        }
+        temp-=4;
+    }
+
+    if (PORTCbits.RC1 == 0) {
+        if (temp == 38) {
+            temp = 38;
+        }
+        temp+=4;
+    }
+
 }
