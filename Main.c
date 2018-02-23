eef3ebf61bcfce2b2afa5ac93d4c9c3eb601562f
MushuLeDragon <dragon.mushu@hotmail.com>
2018-02-23 15:49:48 +0100

    gestion affichage température

--- Main.c
+++ Main.c
@@ -7,6 +7,10 @@
 int tempdebase = 30;
 int temp = tempdebase;
 
+
+void changeTemperature();
+void afficheTemperature();
+
 void main(void) {
 
     //Boutons de température en entrée
@@ -30,24 +34,44 @@ void main(void) {
     //Allume la LED du port RC3
     //PORTCbits.RC3 = 1;
 
+    while (1) {
+        //Mettre les foctions a appeler
+        //tempcapteurs recus
+        //régulateur de température
+        changeTemperature();
+        afficheTemperature(temp);
+    }
 
 
     return;
 }
 
 void changeTemperature() {
-    if (PORTCbits.RC0 == 0) {
-        if (temp == 22) {
-            temp = 22;
-        }
-        temp-=4;
+    if (PORTCbits.RC0 == 0 && temp > 22) {
+        temp -= 4;
     }
 
-    if (PORTCbits.RC1 == 0) {
-        if (temp == 38) {
-            temp = 38;
-        }
-        temp+=4;
+    if (PORTCbits.RC1 == 0 && temp < 38) {
+        temp += 4;
     }
+}
 
+void afficheTemperature(int temp) {
+    switch (temp) {
+        case 22:
+            PORTCbits.RC3 = 1;
+            break;
+        case 26:
+            PORTCbits.RC4 = 1;
+            break;
+        case 30:
+            PORTCbits.RC5 = 1;
+            break;
+        case 34:
+            PORTCbits.RC6 = 1;
+            break;
+        case 38:
+            PORTCbits.RC7 = 1;
+            break;
+    }
 }
